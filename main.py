1546d5060b9e9c1a9da2b8d9d660d40ffefe88e0
Konrad Komorowski <konrad@komorowski.me>
2020-02-24 18:47:43 +0100

    add a hello world file (#4)

---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+
+import typing_extensions
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Hello world!")
+
+logging.info(f"Installed the following package: {typing_extensions}")
